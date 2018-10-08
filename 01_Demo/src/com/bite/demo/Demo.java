762bc7ec8f5066402580ae94f17fd37b1c653c04
jinligithub <1018487750@qq.com>
2018-10-08 15:13:50 +0800

    from IDEA2!

--- 01_Demo/src/com/bite/demo/Demo.java
+++ 01_Demo/src/com/bite/demo/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int i = sc.nextInt();
+        kkkk
         System.out.println(i+100);
     }
 }
