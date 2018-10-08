f5ddb7d480593c8882ddcdb3c9700e10d3b96f4c
jinligithub <1018487750@qq.com>
2018-10-08 15:04:03 +0800

    from IDEA!

---
+++ 01_Demo/src/com/bite/demo/Demo.java
@@ -0,0 +1,10 @@
+package com.bite.demo;
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println(i+100);
+    }
+}
